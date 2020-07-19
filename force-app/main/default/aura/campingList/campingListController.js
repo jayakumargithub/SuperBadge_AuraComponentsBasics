e2e39cbf610397a33bdb6c1ae92c90bcb4f1fbb3
Jayakumar <jayakumar.m.h@gmail.com>
2020-07-19 09:51:30 +0100

    Connect to salesforce with server side controllers

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,4 +1,20 @@
 ({
+    
+    doInit : function(component, event, helper) {
+        alert(1);
+        var action = component.get("c.getItems");
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            alert(state);
+            if(component.isValid() && state === 'SUCCESS'){
+                component.set("v.items", response.getReturnValue()); 
+            }
+    
+        });
+        $A.enqueueAction(action);
+        
+        
+    },
     clickCreateItem : function(component, event, helper) {
         
         var validCampin = component.find('campinForm').reduce(function(validSoFar, inputComp){
@@ -6,12 +22,10 @@
             return validSoFar && inputComp.get('v.validity').valid;
         }, true);
         if(validCampin){
-           var newCampinItem = component.get("v.newItem");
-           var campings = component.get("v.items"); 
-           campings.push(newCampinItem);
-           component.set("v.items", campings); 
+           var newCampinItem = component.get("v.newItem"); 
+            helper.createItem(component,newCampinItem);
            component.set("v.newItem", {'sobjectType':'Camping_Item__c','Name':'', 'Price__c':0,'Quantity__c':0,'Packed__c':false});
-         console.log(campings);
+         
         }
 
     }
--- force-app/main/default/aura/campingList/campingListHelper.js
+++ force-app/main/default/aura/campingList/campingListHelper.js
@@ -1,5 +1,17 @@
 ({
-    helperMethod : function() {
+    createItem : function(component,item) {
+        
+        var action = component.get('c.saveItem');
+        action.setParams({"item":item});
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if(state === "SUCCESS"){
+            var campings = component.get("v.items");
+            campings.push(response.getReturnValue());
+            component.set("v.items", campings);
+            }
+        });
+        $A.enqueueAction(action);
 
     }
 })
